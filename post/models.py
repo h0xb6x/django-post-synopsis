ca9a39468103913f73a4286eef073189367f47f0
h0xb6x <h0xb6x@gmail.com>
2022-09-01 19:58:13 +0500

    add models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    synopsis = models.OneToOneField('Synopsis', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Synopsis(models.Model):
+    text = models.TextField()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=255)
